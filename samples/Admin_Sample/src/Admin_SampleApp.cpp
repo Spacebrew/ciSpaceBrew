90794786f69b51a0c9cc90a8ad47b3a85f8c8533
ryanbartley <ryanbartley@gmail.com>
2013-06-17 17:31:17 -0700

    Route builds and Executes but I haven't been able to test it completely

--- samples/Admin_Sample/src/Admin_SampleApp.cpp
+++ samples/Admin_Sample/src/Admin_SampleApp.cpp
@@ -1,5 +1,8 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+#include <iostream>
+
+#include "ciSpaceBrew.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -11,14 +14,39 @@ class Admin_SampleApp : public AppNative {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+    
+    void onClientConnect( Spacebrew::Config & e ) { cout << "OnClientConnect" << e.name << endl; }
+    void onClientUpdated( Spacebrew::Config & e ) { cout << "OnClientUpdated" << e.name << endl; }
+    void onClientDisconnect( Spacebrew::Config & e ) { cout << "OnclientDisconnect" << e.name << endl; }
+    void onRouteAdded( Spacebrew::Route & e ) { cout << "OnRouteAdded" << e.getPubEnd().name << endl; }
+    void onRouteRemoved( Spacebrew::Route & e ) { cout << "onRouteRemoved" << e.getPubEnd().name << endl; }
+    void onDataPublished( Spacebrew::DataMessage & e ) { cout << "onDataPublished" << e.clientName << endl; }
+    
+    void onMessage( Spacebrew::Message & m ) { cout << "onMessage" << endl; }
+    
+    Spacebrew::AdminConnection spacebrewAdmin;
 };
 
 void Admin_SampleApp::setup()
 {
+    spacebrewAdmin.addPublish("Mouse X", Spacebrew::TYPE_RANGE);
+    spacebrewAdmin.addPublish("Mouse Y", Spacebrew::TYPE_RANGE);
+    spacebrewAdmin.addSubscribe("Values", Spacebrew::TYPE_RANGE);
+    spacebrewAdmin.connect(this, Spacebrew::SPACEBREW_CLOUD, "Cinder Admin App");
+    spacebrewAdmin.addListener(&Admin_SampleApp::onMessage, this);
+    spacebrewAdmin.addClientConnect(&Admin_SampleApp::onClientConnect, this);
+    spacebrewAdmin.addClientUpdate(&Admin_SampleApp::onClientUpdated, this);
+    spacebrewAdmin.addClientDisconnect(&Admin_SampleApp::onClientDisconnect, this);
+    spacebrewAdmin.addRouteAdded(&Admin_SampleApp::onRouteAdded, this);
+    spacebrewAdmin.addRouteRemoved(&Admin_SampleApp::onRouteRemoved, this);
+    spacebrewAdmin.addDataPublisher(&Admin_SampleApp::onDataPublished, this);
+    
 }
 
 void Admin_SampleApp::mouseDown( MouseEvent event )
 {
+    spacebrewAdmin.sendRange("Values", event.getPos().x);
+    spacebrewAdmin.sendRange("Values", event.getPos().y);
 }
 
 void Admin_SampleApp::update()
